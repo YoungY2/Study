5e96f67a3bea23a0b4a1fe65abe851273527fd70
Wang yangyang <youngy@petalmail.com>
2024-03-24 19:52:57 +0800

    b

---
+++ assets/pages_JS.md.CK4U8kEX.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as i,o as a,a3 as n}from"./chunks/framework.BI06Adbe.js";const F=JSON.parse('{"title":"JS","description":"","frontmatter":{},"headers":[],"relativePath":"pages/JS.md","filePath":"pages/JS.md"}'),h={name:"pages/JS.md"},k=n("",71),l=[k];function p(t,e,E,r,d,g){return a(),i("div",null,l)}const o=s(h,[["render",p]]);export{F as __pageData,o as default};
